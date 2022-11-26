97a8688f16258163896d7a4a98a9d0b59983d5d0
charlesdev771 <charlesdev07721@proton.me>
2022-11-26 19:54:30 -0300

    final 1.0

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get('/person/:id', async (req, res) => {
         const person = await Person.findOne({ _id: id })
 
         if (!person) {
-            res.status(422).json({ message: 'Usuário não encontrado!' })
+            res.status(422).json({ message: 'Not found!' })
             return
         }
 
@@ -80,6 +80,69 @@ app.get('/person/:id', async (req, res) => {
         res.status(500).json({ erro: error })
     }
 })
+
+
+app.get('/person/:id', async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const person = await Person.findOne({ _id: id })
+
+        if (!person) {
+            res.status(422).json({ message: 'Not found!' })
+            return
+        }
+
+        res.status(200).json(person)
+    } catch (error) {
+        res.status(500).json({ erro: error })
+    }
+})
+
+app.patch('/person/:id', async (req, res) => {
+    const id = req.params.id
+
+    const { name, salary, approved } = req.body
+
+    const person = {
+        name,
+        salary,
+        approved,
+    }
+
+    try {
+        const updatedPerson = await Person.updateOne({ _id: id }, person)
+
+        if (updatedPerson.matchedCount === 0) {
+            res.status(422).json({ message: 'Not found!' })
+            return
+        }
+
+        res.status(200).json(person)
+    } catch (error) {
+        res.status(500).json({ erro: error })
+    }
+})
+
+app.delete('/person/:id', async (req, res) => {
+    const id = req.params.id
+
+    const person = await Person.findOne({ _id: id })
+
+    if (!person) {
+        res.status(422).json({ message: 'Not found!' })
+        return
+    }
+
+    try {
+        await Person.deleteOne({ _id: id })
+
+        res.status(200).json({ message: 'Removed!' })
+    } catch (error) {
+        res.status(500).json({ erro: error })
+    }
+})
+
 const DB_USER = "ccod";
 const DB_PASSWORD = encodeURIComponent('6VEtZfkLosxcReo3');
 

fbfe115e77461903140d70fc23af1211a799c8d3
charlesdev771 <charlesdev07721@proton.me>
2022-11-26 18:44:02 -0300

    DB, model

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+//6VEtZfkLosxcReo3 PASS
+//ccod user
+// mongodb+srv://ccod:6VEtZfkLosxcReo3@myapy.swphhqr.mongodb.net/?retryWrites=true&w=majority
+
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const Person = require("./models/Person");
 
 app.use(
     express.urlencoded({
@@ -15,4 +22,20 @@ app.get('/', (req, res) =>{
 
 })
 
-app.listen(3000);
+const DB_USER = "ccod";
+const DB_PASSWORD = encodeURIComponent('6VEtZfkLosxcReo3');
+
+
+mongoose.connect(
+    'mongodb+srv://ccod:6VEtZfkLosxcReo3@myapy.swphhqr.mongodb.net/?retryWrites=true&w=majority'
+    ).then(() => {
+
+        console.log("Conected");
+        app.listen(3000);
+
+    }).catch((err) => console.log(err)
+
+    )
+
+
+app.listen(4000);
